73e12bcbb5ba42884f832260812c2b28f974a232
eduard-ops <epustynnik@gmail.com>
2022-06-08 14:03:24 +0300

    Movies-list.js

---
+++ src/pages/Movies-list.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import { toast } from 'react-toastify';
+
+import { fetchMoviesByName } from 'services/movies-api';
+
+import Loader from 'components/Loader';
+
+export default function MoviesList({ movieName }) {
+  const [movies, setMovies] = useState(null);
+  const [spinner, setSpiner] = useState(false);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      setSpiner(true);
+      try {
+        const { results } = await fetchMoviesByName(movieName);
+        if (results.length <= 0) {
+          throw new Error();
+        }
+        setMovies(results);
+        setSpiner(false);
+      } catch (error) {
+        toast.info(`Image with name ${movieName} not found 😢`, {
+          theme: 'colored',
+        });
+      }
+    };
+    fetchMovie();
+  }, [movieName]);
+
+  return (
+    <>
+      {spinner && <Loader />}
+      {movies && (
+        <ul>
+          {movies.map(({ original_title, id }) => {
+            return (
+              <li key={id}>
+                <Link
+                  to={`${id}`}
+                  state={{ pathname: `/movies?query=${movieName}` }}
+                >
+                  {original_title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
+}
